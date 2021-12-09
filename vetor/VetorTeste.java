3d74458d9eefc5c1625c274e17695541c526dfae
Raquel Rodrigues <raquel.mr.rodrigues@gmail.com>
2021-12-09 10:25:53 -0300

    vetor

---
+++ vetor/VetorTeste.java
@@ -0,0 +1,19 @@
+package vetor;
+
+public class VetorTeste {
+	
+	public static void main(String[] args) {
+		Aluno a1 = new Aluno("João");
+		Aluno a2 = new Aluno("José");
+		Aluno a3 = new Aluno("Danilo");
+		
+		Vetor lista = new Vetor();
+		
+		lista.adiciona(a1);
+		lista.adiciona(a2);
+		
+		System.out.println(lista);
+		
+	}
+
+}
